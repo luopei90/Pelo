4931d77d29db8452574fdbc32a0fefaf9662a092
luopei90 <luopei2009@163.com>
2015-01-19 10:59:40 +0800

    update main

--- src/gitdemo/Test.java
+++ src/gitdemo/Test.java
@@ -1,7 +1,12 @@
 package gitdemo;
 
 public class Test {
+
 	public static void main(String args[]) {
+		sayHello();
+	}
+
+	public static void sayHello() {
 		System.out.println("hello world");
 	}
 }
